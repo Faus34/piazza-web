6cf2d9ebf6542b2269101d4d7c6f070d6c86a3fa
Faustino <faustino@MEXL007870.local>
2023-04-29 11:44:33 -0600

    Set up user, organization, and membership models

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_164742) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "memberships", "organizations"
